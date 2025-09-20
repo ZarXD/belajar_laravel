2aa686dbe54043de26b0437b26fa5edcb71ed53d
ZarXD <banghads09@gmail.com>
2025-09-20 09:51:43 +0700

    Menggunakan Laravel Breeze

--- routes/web.php
+++ routes/web.php
@@ -1,27 +1,20 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\MahasiswaController;
-use App\Http\Controllers\MatakuliahController;
-use App\Http\Controllers\KelasController;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/hello',  function () {
-    return "Hello World!";
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
-Route::get('/nama', function () {
-    return "Nama saya : izaa";
-});
-
-Route::get('/mahasiswa', [MahasiswaController::class, 'index']);
-Route::post('/mahasiswa', [MahasiswaController::class, 'store']);
-
-Route::get('/matakuliah', [MatakuliahController::class, 'index']);
-Route::post('/matakuliah', [MatakuliahController::class, 'store']);
-
-Route::get('/kelas', [KelasController::class, 'index']);
-Route::post('/kelas', [KelasController::class, 'store']);
+require __DIR__.'/auth.php';
